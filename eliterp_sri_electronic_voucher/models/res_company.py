e343ac8fbddd0755a7c5a86df0d80449c85cbf1c
Ing. Mario Rangel <sistemas@elitumgroup.com>
2020-01-29 15:37:37 -0500

    [ADD] Nuevo campo para email (RIDE).

--- eliterp_sri_electronic_voucher/models/res_company.py
+++ eliterp_sri_electronic_voucher/models/res_company.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+import re
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+EMAIL_PATTERN = '([^ ,;<@]+@[^> ,;]+)'
 
 
 class Partner(models.Model):
     _inherit = 'res.partner'
 
+    @api.constrains('email')
+    def _check_email(self):
+        if not re.match(EMAIL_PATTERN, self.email):
+            raise ValidationError(_("Email esta en un formato inválido!"))
+
+    @api.constrains('email_optional')
+    def _check_email_optional(self):
+        if not re.match(EMAIL_PATTERN, self.email_optional):
+            raise ValidationError(_("Email opcional esta en un formato inválido!"))
+
     email = fields.Char(required=True)
+    email_optional = fields.Char('Email opcional',
+                                 help="Campo de email opcional para el envío del RIDE.")
 
 
 class Company(models.Model):
@@ -41,3 +57,8 @@ class Company(models.Model):
     )
     email_voucher_electronic = fields.Char('Correo para facturación electrónica',
                                            help="Técnico: si no se coloca correo toma el configurado en compañía.")
+
+    @api.constrains('email_voucher_electronic')
+    def _check_email_voucher_electronic(self):
+        if not re.match(EMAIL_PATTERN, self.email_voucher_electronic):
+            raise ValidationError(_("Email opcional esta en un formato inválido!"))
